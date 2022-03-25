338d2f1aa464585e5aa4676499e8a684a09801d2
ncerber <ncerber@yandex.ru>
2022-03-25 14:53:22 +0500

    Добавлен класс Bishop для реализации логики для слона(офицера).

---
+++ src/Bishop.java
@@ -0,0 +1,32 @@
+public class Bishop extends ChessPiece {
+    public Bishop(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (!isPositionCorrect(line, column, toLine, toColumn)) return false;
+
+        boolean isRight = column < toColumn, isTop = line < toLine;
+        int vIterator = isTop ? 1 : -1, gIterator = isRight ? 1 : -1;
+        int tLine = line, tColumn = column;
+        for (int i = 0; i < 8; i++) {
+            tLine += vIterator;
+            tColumn += gIterator;
+            if (tLine == toLine && tColumn == toColumn) return true;
+            if (tLine == toLine | tColumn == toColumn) return false;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "B";
+    }
+}
