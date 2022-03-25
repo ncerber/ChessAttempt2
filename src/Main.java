faba3019f1f40fb302a83648ca18693b39b4c472
ncerber <ncerber@yandex.ru>
2022-03-25 18:46:12 +0500

    Добавлены тесты для класса Rook.

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,21 @@ public class Main {
     public static void main(String[] args) {
         int line,column, toLine, toColumn;
         ChessBoard board = new ChessBoard("White");
-        Bishop bishop = new Bishop("White");
+        Rook rook = new Rook("White");
 
-        line=3; column=3; toLine=5; toColumn=5;
-        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+bishop.canMoveToPosition(board,line,column,toLine,toColumn));
+        line=3; column=3; toLine=3; toColumn=5;
+        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+rook.canMoveToPosition(board,line,column,toLine,toColumn));
 
-        line=3; column=3; toLine=5; toColumn=1;
-        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+bishop.canMoveToPosition(board,line,column,toLine,toColumn));
+        line=3; column=3; toLine=3; toColumn=0;
+        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+rook.canMoveToPosition(board,line,column,toLine,toColumn));
 
-        line=3; column=3; toLine=0; toColumn=0;
-        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+bishop.canMoveToPosition(board,line,column,toLine,toColumn));
+        line=3; column=3; toLine=5; toColumn=3;
+        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+rook.canMoveToPosition(board,line,column,toLine,toColumn));
 
-        line=3; column=3; toLine=0; toColumn=6;
-        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+bishop.canMoveToPosition(board,line,column,toLine,toColumn));
+        line=3; column=3; toLine=0; toColumn=3;
+        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+rook.canMoveToPosition(board,line,column,toLine,toColumn));
 
-        line=3; column=3; toLine=1; toColumn=6;
-        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+bishop.canMoveToPosition(board,line,column,toLine,toColumn));
+        line=3; column=3; toLine=4; toColumn=1;
+        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+rook.canMoveToPosition(board,line,column,toLine,toColumn));
     }
 }
