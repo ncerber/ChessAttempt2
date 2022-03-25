75ff05424c8f535f4974141f37fcd416de157817
ncerber <ncerber@yandex.ru>
2022-03-25 12:43:43 +0500

    Добавлена функция isPositionCorrect которая проверяет чтобы координаты не выходили за пределф игрового поля.

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -9,4 +9,10 @@ public abstract class ChessPiece {
     public abstract String getColor();
     public abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
     public abstract String getSymbol();
+
+    public boolean isPositionCorrect(int line, int column, int toLine, int toColumn){
+        if (toLine > 7 | toLine < 0) return false;
+        if (toColumn > 7 | toColumn < 0) return false;
+        return line != toLine || column != toColumn;
+    }
 }
