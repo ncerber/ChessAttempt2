3d480a2ee55ede7b4ae9eed0eade98cae4565a8a
ncerber <ncerber@yandex.ru>
2022-03-26 00:30:47 +0500

    Тесты для атаки на короля

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,51 @@
 public class Main {
+    public static void printTest(ChessBoard board,King king, ChessPiece piece, int line, int column){
+
+        board.board[line][column] = piece;
+        boolean isAttack = king.isUnderAttack(board, 3, 3);
+        System.out.println(piece.getClass().getName()+" on line "+line+" and column "+column+".  Is attack: "+isAttack+". it`s "+(isAttack?"right":"wrong"));
+        board.board[line][column] = null;
+    }
     public static void main(String[] args) {
         int line,column, toLine, toColumn;
+        boolean isAttack;
         ChessBoard board = new ChessBoard("White");
-        Rook rook = new Rook("White");
 
-        line=3; column=3; toLine=3; toColumn=5;
-        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+rook.canMoveToPosition(board,line,column,toLine,toColumn));
+        Queen queen = new Queen("Black");
+        Rook rook = new Rook("Black");
+        Bishop bishop = new Bishop("Black");
+        Pawn pawn = new Pawn("Black");
+        Horse horse = new Horse("Black");
 
-        line=3; column=3; toLine=3; toColumn=0;
-        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+rook.canMoveToPosition(board,line,column,toLine,toColumn));
+        King king = new King("White");
+        board.board[3][3] = king;
 
-        line=3; column=3; toLine=5; toColumn=3;
-        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+rook.canMoveToPosition(board,line,column,toLine,toColumn));
+        printTest(board,king,rook,0,3);
+        printTest(board,king,queen,3,5);
+        printTest(board,king,queen,5,3);
+        printTest(board,king,rook,3,1);
 
-        line=3; column=3; toLine=0; toColumn=3;
-        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+rook.canMoveToPosition(board,line,column,toLine,toColumn));
+        System.out.println("diagonals");
+        //diagonals
+        printTest(board,king,queen,1,5);
+        printTest(board,king,bishop,5,5);
+        printTest(board,king,queen,5,1);
+        printTest(board,king,bishop,1,1);
 
-        line=3; column=3; toLine=4; toColumn=1;
-        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+rook.canMoveToPosition(board,line,column,toLine,toColumn));
+        printTest(board,king,pawn,2,2);
+        printTest(board,king,pawn,2,4);
+        printTest(board,king,pawn,4,2);
+        printTest(board,king,pawn,4,4);
+
+        //horse
+        System.out.println("horse");
+        printTest(board,king,horse,5,4);
+        printTest(board,king,horse,4,5);
+        printTest(board,king,horse,2,5);
+        printTest(board,king,horse,1,4);
+        printTest(board,king,horse,1,2);
+        printTest(board,king,horse,2,1);
+        printTest(board,king,horse,4,1);
+        printTest(board,king,horse,5,2);
     }
 }
