3c4513d644b476fbfce3a3c460d5d7ef7f86c9c8
ncerber <ncerber@yandex.ru>
2022-03-25 16:22:31 +0500

    Добавлены тесты для класса Bishop.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,21 @@ public class Main {
     public static void main(String[] args) {
         int line,column, toLine, toColumn;
         ChessBoard board = new ChessBoard("White");
-        Pawn pawn = new Pawn("White");
+        Bishop bishop = new Bishop("White");
 
-        line=1; column=0; toLine=2; toColumn=1;
-        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+pawn.canMoveToPosition(board,line,column,toLine,toColumn));
+        line=3; column=3; toLine=5; toColumn=5;
+        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+bishop.canMoveToPosition(board,line,column,toLine,toColumn));
+
+        line=3; column=3; toLine=5; toColumn=1;
+        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+bishop.canMoveToPosition(board,line,column,toLine,toColumn));
+
+        line=3; column=3; toLine=0; toColumn=0;
+        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+bishop.canMoveToPosition(board,line,column,toLine,toColumn));
+
+        line=3; column=3; toLine=0; toColumn=6;
+        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+bishop.canMoveToPosition(board,line,column,toLine,toColumn));
+
+        line=3; column=3; toLine=1; toColumn=6;
+        System.out.println("line"+line+"; column:"+column+"; toLine:"+toLine+"; toColumn:"+toColumn+"; canMove:"+bishop.canMoveToPosition(board,line,column,toLine,toColumn));
     }
 }
