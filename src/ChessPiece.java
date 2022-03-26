281c329dbc3a710a6cacf9c6e82612f56e1d726e
ncerber <ncerber@yandex.ru>
2022-03-27 00:07:15 +0500

    Изменено оформление кода. Добавлена функция isRightDestination

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -8,12 +8,19 @@ public abstract class ChessPiece {
     }
 
     public abstract String getColor();
+
     public abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+
     public abstract String getSymbol();
 
-    public boolean isPositionCorrect(int line, int column, int toLine, int toColumn){
+    public boolean isPositionCorrect(int line, int column, int toLine, int toColumn) {
         if (toLine > 7 | toLine < 0) return false;
         if (toColumn > 7 | toColumn < 0) return false;
         return line != toLine || column != toColumn;
+    }
+
+    public boolean isRightDestination(ChessBoard board, int line, int column) {
+        if (board.board[line][column] == null) return true;
+        return !board.board[line][column].getColor().equals(color);
     }
 }
